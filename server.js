78d410a8b7ad61334e03b0ccb13e7659b5933a1a
prorishi <3rishi11@gmail.com>
2021-02-25 13:14:21 +0530

    initial

---
+++ server.js
@@ -0,0 +1,10 @@
+const { response } = require("express");
+const express = require("express");
+
+const server = express();
+
+server.get("/", (request, response) => {
+    response.end("hello");
+});
+
+server.listen(80);
